4f713f7de8fc23d4b5e8d9124a6ca78bc6875873
tohsen1 <mms2003saad@gmail.com>
2023-02-17 03:57:22 +0200

    a

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
  * my 
  * life 
  * i want to day */
-int main()
+int main(void)
 {
 
-printf("with proper grammar, but the outcome is a piece of art, ");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 
 return(0);
 }	
