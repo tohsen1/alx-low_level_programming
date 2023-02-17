3657eb3aadf112ddbf49c38831beae25535593e6
tohsen1 <mms2003saad@gmail.com>
2023-02-17 04:02:07 +0200

    a

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,15 @@
-#include  <stdio.h>
-/*i hate 
+#include <stdio.h>
+/**
+ * i hate 
  * my 
  * life 
- * i want to day */
+ * i want to day
+ * and by the way this a function that out that string 
+ */
 int main(void)
 {
 
-printf("with proper grammar, but the outcome is a piece of art,\n");
+         printf("with proper grammar, but the outcome is a piece of art,\n");
 
-return(0);
+         return(0);
 }	
