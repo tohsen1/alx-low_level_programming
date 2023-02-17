5b95a7c9b2e9b9951d4859f5f542ab94b77ee277
tohsen1 <mms2003saad@gmail.com>
2023-02-17 21:51:45 +0200

    a

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - Entry point
  *
@@ -18,4 +17,3 @@ int main(void)
 
 	return (0);
 }
-
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,13 +10,18 @@
 
 int main(void)
 {
-	char low;
+	char low, e, q;
+
+	e = 'e';
+	q = 'q';
 
 	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-	for (low = 'A'; low <= 'Z'; low++)
-		putchar(low);
+	{
+		if (low != e && low != q)
+			putchar(low);
+	}
 	putchar('\n');
 
 	return (0);
 }
+
